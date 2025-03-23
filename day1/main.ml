8984cbc4ef9c0b6d203a0f232e28f91db49f18ac
James Meyers <james.meyers212@gmail.com>
2025-03-23 16:28:59 -0400

    remove stale thing

--- day1/main.ml
+++ day1/main.ml
@@ -211,19 +211,6 @@ let generate_structs definitions =
   |> snd
   |> List.rev
 
-(* let logger_defn = {|template <typename TStruct>
-class json_logger
-{
-    std::string _buf{TStruct::empty()};
-public:
-    json_logger() {}
-
-    void log(const TStruct& t) {
-        t.format(_buf.data());
-        std::cout << _buf << std::endl;
-    }
-};|} *)
-
 let () = 
 parse_yaml "/users/jamesmeyers/life/test_file.yaml"
 |> read_structs
